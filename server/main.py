eac537937ed3f2a26d84d4a7b9fdcd8920427c2c
Deepikavusarthi <123539808+Deepikavusarthi@users.noreply.github.com>
2024-11-07 04:32:07 -0500

    Initial (#3)

---
+++ server/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from server.database import engine, Base
+from server.routes.user import router as user_router
+from server.routes.mood import router as mood_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(user_router, prefix="/api", tags=["users"])
+app.include_router(mood_router, prefix="/api/moods", tags=["moods"])  # Include mood routes
+# Include other routers similarly
